bc7b77d450389935742aae4a587109363ec789a6
gazoblock <pasha.m@lineir.me>
2022-01-28 15:51:32 +0200

    sync

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,86 +1,39 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  ERC20,
-  Approval,
-  Burn,
-  Mint,
-  Swap,
-  Sync,
-  Transfer
-} from "../generated/ERC20/ERC20"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt, Address, store, log } from '@graphprotocol/graph-ts';
+import { Transfer, ERC20 } from '../generated/ERC20/ERC20';
+import { Token, Holder } from '../generated/schema';
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+function updateBalance(tokenAddress: Address, holderAddress: Address, value: BigInt, increase: boolean): void {
+  if (holderAddress.toHexString() == '0x0000000000000000000000000000000000000000') return;
+  let id = tokenAddress.toHex() + '-' + holderAddress.toHex();
+  let holder = Holder.load(id);
+  if (holder == null) {
+    holder = new Holder(id);
+    holder.address = holderAddress;
+    holder.balance = BigInt.fromI32(0);
+    holder.token = tokenAddress.toHex();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.MINIMUM_LIQUIDITY(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.burn(...)
-  // - contract.decimals(...)
-  // - contract.devFee(...)
-  // - contract.factory(...)
-  // - contract.getReserves(...)
-  // - contract.kLast(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.price0CumulativeLast(...)
-  // - contract.price1CumulativeLast(...)
-  // - contract.swapFee(...)
-  // - contract.symbol(...)
-  // - contract.token0(...)
-  // - contract.token1(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  holder.balance = increase ? holder.balance.plus(value) : holder.balance.minus(value);
+  if (holder.balance.isZero()) {
+    store.remove('Holder', id);
+  } else {
+    holder.save();
+  }
 }
 
-export function handleBurn(event: Burn): void {}
+function updateTotalSupply(address: Address): void {
+  let contract = ERC20.bind(address);
+  let token = Token.load(address.toHex());
+  if (token == null) {
+    token = new Token(address.toHex());
+    token.address = address;
+    token.totalSupply = BigInt.fromI32(0);
+  }
+  token.totalSupply = contract.totalSupply();
+  token.save();
+}
 
-export function handleMint(event: Mint): void {}
-
-export function handleSwap(event: Swap): void {}
-
-export function handleSync(event: Sync): void {}
-
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  updateTotalSupply(event.address);
+  updateBalance(event.address, event.params.from, event.params.value, false);
+  updateBalance(event.address, event.params.to, event.params.value, true);
+}
